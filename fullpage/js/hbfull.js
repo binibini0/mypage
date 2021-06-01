5b134124664a615488bfe917d4494af0ad8cf279
binibini0 <dhgyqls11@naver.com>
2021-06-01 20:47:44 +0900

    fullpage 네비게이션 셋팅

---
+++ fullpage/js/hbfull.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('#hbfull').fullpage({
+  		//options here
+  		autoScrolling:true,
+  		scrollHorizontally: true,
+      sectionsColor : ['pink','white','black','orange'],
+      navigation: true,
+    	navigationPosition: 'right',
+    	navigationTooltips: ['나의 소개', '포폴','사전면접'],
+      showActiveTooltip: true,
+  	});
+});
